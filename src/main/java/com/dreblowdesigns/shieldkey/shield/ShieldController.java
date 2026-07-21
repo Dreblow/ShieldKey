54b2f409a6da5ccf5d51ebc45d381c7daf899fe2
Derek Dreblow <derek.dreblow@gmail.com>
2026-07-20 22:09:44 -0700

    v0.0.5 - fully functioning shield key

--- src/main/java/com/dreblowdesigns/shieldkey/shield/ShieldController.java
+++ src/main/java/com/dreblowdesigns/shieldkey/shield/ShieldController.java
@@ -1,6 +1,8 @@
 package com.dreblowdesigns.shieldkey.shield;
 
 import com.dreblowdesigns.shieldkey.ShieldKeyClient;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.Items;
 
 public final class ShieldController {
 
@@ -16,10 +18,22 @@ public final class ShieldController {
 
         shieldRequested = requested;
 
+        Minecraft client = Minecraft.getInstance();
+
+        if (client.player == null || client.gameMode == null) {
+            return;
+        }
+
         if (shieldRequested) {
-            ShieldKeyClient.LOGGER.info("Shield key pressed.");
+            if (client.player.getOffhandItem().is(Items.SHIELD)) {
+                ShieldKeyClient.LOGGER.info("Shield key pressed. Starting shield use.");
+                client.options.keyUse.setDown(true);
+            } else {
+                ShieldKeyClient.LOGGER.info("Shield key pressed, but no shield in offhand.");
+            }
         } else {
-            ShieldKeyClient.LOGGER.info("Shield key released.");
+            ShieldKeyClient.LOGGER.info("Shield key released. Stopping shield use.");
+            client.options.keyUse.setDown(false);
         }
     }
 
